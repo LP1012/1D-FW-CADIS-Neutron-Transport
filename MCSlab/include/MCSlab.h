332e9c0e514aac975b70b67bbaf6b77a0e6275d2
Liam Pohlmann <lpgeorge2@gmail.com>
2025-09-17 21:29:19 -0500

    create constructor for MCSlab, create source method for k_eigenvalue simulation

--- MCSlab/include/MCSlab.h
+++ MCSlab/include/MCSlab.h
@@ -1,33 +1,33 @@
 #pragma once
 
-#include <vector>
 #include "Neutron.h"
 #include "Point.h"
+#include <vector>
 
-class MCSlab
-{
+class MCSlab {
 public:
-	/// method to run simulation
-	void k_eigenvalue();
+  MCSlab(const unsigned int x_cells, const unsigned int n_particles,
+         const unsigned int n_generations, const unsigned int n_inactive);
+  /// method to run simulation
+  void k_eigenvalue();
 
-	
 protected:
-	/// mesh size for Shannon entropy calcs
-	const unsigned int _x_cells;
-	
-	/// number of neutrons per generation
-	const unsigned int _n_particles;
-	/// number of generations
-	const unsigned int _n_generations;
+  /// mesh size for Shannon entropy calcs
+  const unsigned int _x_cells;
 
-	/// vector of slab data: size, Sigma_a, Sigma_s, nu_Sigma_f
-	const std::vector<std::vector<double>> _slabs;
-	
-	/// bank of source sites
-	std::vector<Neutron> _fission_bank;
+  /// number of neutrons per generation
+  const unsigned int _n_particles;
+  /// number of generations
+  const unsigned int _n_generations;
+  /// number of inactive cycles
+  const unsigned int _n_inactive;
 
-	/// flux at each point in mesh
-	std::vector<double> _scalar_flux;
+  /// vector of slab data: size, Sigma_a, Sigma_s, nu_Sigma_f
+  const std::vector<std::vector<double>> _slabs;
 
-	
+  /// bank of source sites
+  std::vector<Neutron> _fission_bank;
+
+  /// flux at each point in mesh
+  std::vector<double> _scalar_flux;
 };
