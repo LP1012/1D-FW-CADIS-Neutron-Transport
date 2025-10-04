0bcd4d53b33f007e35bb78731b18482fb7a05d57
Liam Pohlmann <lpgeorge2@gmail.com>
2025-10-04 09:57:17 -0500

    add test for number of regions in sim

--- MCSlab/tests/simulation_test.cpp
+++ MCSlab/tests/simulation_test.cpp
@@ -18,6 +18,7 @@ protected:
 
   // define test-only getters
   unsigned int getNTotalCells(MCSlab sim) const { return sim._n_total_cells; }
+  std::vector<Region> regions(MCSlab sim) const { return sim._regions; }
 };
 
 TEST_F(MCSlabTest, InitializeSimulation) {
@@ -27,4 +28,10 @@ TEST_F(MCSlabTest, InitializeSimulation) {
     EXPECT_EQ(sim.nInactive(), 2);
   }
   EXPECT_EQ(getNTotalCells(test_sim_1), 10);
+  EXPECT_EQ(getNTotalCells(test_sim_2), 16);
+  EXPECT_EQ(getNTotalCells(test_sim_split), 26);
+
+  for (auto i = 0; i < test_sims.size(); i++) {
+    EXPECT_EQ(regions(test_sims[i]).size(), i + 1);
+  }
 }
