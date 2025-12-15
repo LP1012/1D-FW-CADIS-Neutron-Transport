93399a64b73ac84e20837938966e2be149e313c0
Liam Pohlmann <liam.pohlmann@gmail.com>
2025-12-15 00:59:03 -0600

    add quadrature level to constructor to initialize angular flux vectors correctly

--- include/SNCell.h
+++ include/SNCell.h
@@ -5,15 +5,29 @@
 class SNCell : public CellBase
 {
 public:
-  SNCell(const Cell & cell);
+  SNCell(const Cell & cell, const unsigned int gl_quad);
   SNCell(const double xmin,
          const double xmax,
          const double Sigma_a,
          const double Sigma_s,
-         const double nu_Sigma_f);
+         const double nu_Sigma_f,
+         const unsigned int gl_quad);
+
+  void setScalarFlux(const double new_flux) { _scalar_flux = new_flux; }
+  void setSource(const double new_source) { _source = new_source; }
+  void setAngularFlux(const double new_angular_flux, const unsigned int index)
+  {
+    _angular_fluxes[index] = new_angular_flux;
+  }
+
+  const double source() const { return _source; }
+  const double scalarFlux() const { return _scalar_flux; }
 
 protected:
+  const unsigned int _gl_quad;
   double _scalar_flux;
   std::vector<double> _angular_fluxes;
   double _source;
+
+  void initializeAngularFluxVector();
 };
--- src/SNCell.cpp
+++ src/SNCell.cpp
@@ -1,17 +1,28 @@
 #include "SNCell.h"
 
-SNCell::SNCell(const Cell & cell)
-  : CellBase(cell.xMin(), cell.xMax(), cell.sigmaA(), cell.sigmaS(), cell.nuSigmaF())
+SNCell::SNCell(const Cell & cell, const unsigned int gl_quad)
+  : CellBase(cell.xMin(), cell.xMax(), cell.sigmaA(), cell.sigmaS(), cell.nuSigmaF()),
+    _gl_quad(gl_quad)
 {
+  initializeAngularFluxVector();
 }
 
 SNCell::SNCell(const double xmin,
                const double xmax,
                const double Sigma_a,
                const double Sigma_s,
-               const double nu_Sigma_f)
-  : CellBase(xmin, xmax, Sigma_a, Sigma_s, nu_Sigma_f)
+               const double nu_Sigma_f,
+               const unsigned int gl_quad)
+  : CellBase(xmin, xmax, Sigma_a, Sigma_s, nu_Sigma_f), _gl_quad(gl_quad)
 {
+  initializeAngularFluxVector();
   if (Sigma_s + nu_Sigma_f > 1e-15)
     _source = 1.0; // set dummy source value if scattering or fission is present
+}
+
+void
+SNCell::initializeAngularFluxVector()
+{
+  for (auto i = 0; i < _gl_quad; i++)
+    _angular_fluxes.push_back(0.0); // just initialize angular fluxes to 0s
 }
